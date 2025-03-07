0e876e4828390ce69411a60d9b3651d1bd5a5856
LeviathanTurtle <wbwads@gmail.com>
2025-03-06 22:29:45 -0500

    shhh new ones

---
+++ src/empty.js
@@ -0,0 +1,9 @@
+/** @param {NS} ns */
+export async function main(ns) {
+    // As you can probably tell, this script does absolutely nothing, and
+    // is only used for the "Need more real life ram" achievement
+  
+    while (true) {
+      await ns.sleep(1000);
+    }
+  }
